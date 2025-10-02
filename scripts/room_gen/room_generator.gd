2d82e5bca06023ec489679b9eda14dfc44a7b41f
npnorman <npn.not@gmail.com>
2025-10-02 16:36:45 -0400

    added algorithm

--- scripts/room_gen/room_generator.gd
+++ scripts/room_gen/room_generator.gd
@@ -18,8 +18,9 @@ enum Rotation {
 
 # map
 # m x m (m = 10) expand to 11 for overflow
-var m = 10
-var roommap = [
+var current_location:Vector2 = Vector2(0,0)
+var current_room:int = 0
+var roommap:Array = [
 	[[],[],[],[],[],[],[],[],[],[],[]],
 	[[],[],[],[],[],[],[],[],[],[],[]],
 	[[],[],[],[],[],[],[],[],[],[],[]],
@@ -34,25 +35,66 @@ var roommap = [
 ]
 
 var roomstack:Array = []
-var startingRoom = [0,0,ExitType.ONE,Rotation.ZERO]
-var rooms = []
+var starting_room:Vector4 = Vector4(0,0,ExitType.ONE,Rotation.ZERO)
+var rooms:Array = []
 
-func generatorRoom(exit, rotation, count):
-	var rooms = []
+func generateRooms(exit, rotation, count):
+	var rooms:Array = []
 	
 	for i in count:
-		rooms.append([0,0,exit,rotation])
+		rooms.append(Vector4(0,0,exit,rotation))
 	
 	return rooms
 
+func add_room_current_location(room:Vector4):
+	roommap[current_location.x][current_location.y] = room
+
 func generateDeadEnd(exit, rotation):
-	return [0,0,exit,rotation]
+	return Vector4(0,0,exit,rotation)
 
 func _ready() -> void:
-	m = 11
-	var center = int(floor(m / 2))
+	var m = len(roommap)
+	var n = len(roommap[0])
 	
-	var knapsack = generatorRoom(ExitType.FOUR,Rotation.ZERO,5)
+	#define center of room to place start
+	var centerM:int = int(floor(m / 2))
+	var centerN:int = int(floor(n/2))
+	current_location.x = centerM
+	current_location.y = centerN
 	
-	startingRoom.append(roomstack)
-	roommap[center][center] = roomstack
+	#define rooms in bag to pull
+	var knapsack:Array = generateRooms(ExitType.FOUR,Rotation.ZERO,5)
+	
+	roomstack.append(starting_room)
+	add_room_current_location(starting_room)
+	generateMap()
+	
+	for line in roommap:
+		print(line)
+
+func generateMap():
+	# pick a random room in the knapsack
+	# pick a random room in the stack
+	# pick a random exit from that room
+	# check if that spot (x,y) is taken
+		# for each neighbor (if exists)
+			# check if neighbor's exit is being blocked
+				# if it can fit, add to map/stack
+				# otherwise, pick another random spot
+	
+	# if no room can fit, remove the topmost room from stack
+		# try a different room from knapsack
+			# if all rooms cannot fit,
+				# remove rooms from knapsack (ERROR)
+	
+	# after all rooms are placed
+	# for each room
+		# for each exit
+			# if there is no room there
+				# add a dead end (generate one)
+	
+	# after stack is full,
+		# for each room in stack
+			# use 32x32 offset to place room scenes
+			# (rooms should have leftmost corner at 0,0)
+			# (rooms should have centered walkways to connect)
