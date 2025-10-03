e0f20c15c9859645f2e6fb9e55c391e46bbe4f46
npnorman <npn.not@gmail.com>
2025-10-02 23:57:49 -0400

    basic room gen

--- scripts/room_gen/room_generator.gd
+++ scripts/room_gen/room_generator.gd
@@ -1,4 +1,6 @@
-extends Node
+extends Node2D
+
+var test_room:PackedScene = preload("res://scenes/RoomGen/test_room.tscn")
 
 enum ExitType {
 	FOUR,
@@ -51,6 +53,7 @@ func _ready() -> void:
 	#define center of room to place start
 	var centerM:int = int(floor(m / 2))
 	var centerN:int = int(floor(n/2))
+	var center = Vector2(centerM,centerN)
 	current_location.x = centerM
 	current_location.y = centerN
 	
@@ -59,19 +62,66 @@ func _ready() -> void:
 	
 	add_room_current_location(starting_room)
 	generateMap()
+	print("Roomstack")
+	print(roomstack)
+	
+	# after stack is full,
+	display_rooms(center)
 
 func generateMap(seed:int = -1):
 	var rng:RandomNumberGenerator = RandomNumberGenerator.new()
 	if seed != -1:
 		rng.seed = seed
 	
+	while len(knapsack) > 0:
+		pick_room_from_knapsack(rng)
+		
+		# if no room can fit, remove the topmost room from stack
+			# try a different room from knapsack
+				# if all rooms cannot fit,
+					# remove rooms from knapsack (ERROR)
+		
+	# after all rooms are placed
+	# for each room
+		# for each exit
+			# if there is no room there
+				# add a dead end (generate one)
+
+func display_rooms(center:Vector2):
+	for i in range(0,len(roomstack)):
+	# for each room in stack
+		place_room_at_xy(roomstack[i],center)
+		# use 32x32 offset to place room scenes
+		# (rooms should have leftmost corner at 0,0)
+		# (rooms should have centered walkways to connect)
+
+func place_room_at_xy(coords:Vector4,center:Vector2):
+	var tile_offset = 32 * 16 #32 tiles each at 16 pixels
+	
+	var newRoom:Node2D = test_room.instantiate()
+	
+	var adjusted_coords:Vector2 = Vector2(coords.x,coords.y)
+	adjusted_coords -= center
+	adjusted_coords *= tile_offset
+	adjusted_coords.y *= -1
+	
+	newRoom.global_position = adjusted_coords
+	
+	add_child(newRoom)
+	print(get_child_count())
+	print("Room added")
+
+func pick_room_from_knapsack(rng:RandomNumberGenerator):
 	# pick a random room in the knapsack, and remove it
 	var knapsack_index:int = rng.randi_range(0,len(knapsack)-1)
-	var newRoom:Vector4 = knapsack[knapsack_index]
+	var newRoom:Vector4 = knapsack.pop_at(knapsack_index)
 	
 	# pick a random room in the stack
 	var roomstack_index:int = rng.randi_range(0,len(roomstack)-1)
 	var oldRoom:Vector4 = roomstack[roomstack_index]
+	
+	current_location.x = oldRoom.x
+	current_location.y = oldRoom.y
 	
 	# pick a random exit from that room
 	var number_of_exits = get_number_of_exits(oldRoom)
@@ -94,8 +144,13 @@ func generateMap(seed:int = -1):
 	var checkingCoordinates:Vector2 = Vector2(0,0)
 	checkingCoordinates = current_location + offset
 	
+	var is_blocking = false
+	
 	# check if that spot (x,y) is taken
-	if is_room_in_xy(checkingCoordinates) == false:
+	if is_room_in_xy(checkingCoordinates):
+		is_blocking = true
+		
+	else:
 		var neighbors = [
 			Vector2( 1, 0),
 			Vector2(-1, 0),
@@ -106,29 +161,60 @@ func generateMap(seed:int = -1):
 		for i in range(0, len(neighbors)):
 			# check if neighbor's exit is being blocked
 			if is_room_in_xy(current_location + neighbors[i]):
-				pass
-				#get room at xy
-				#get exits of room
-				#if no exits point to this room
-					# it can fit, add to map/stack
-					# otherwise, pick another random spot
+				if is_blocking_exit(current_location, neighbors[i], exits):
+					is_blocking = true
 	
-	# if no room can fit, remove the topmost room from stack
-		# try a different room from knapsack
-			# if all rooms cannot fit,
-				# remove rooms from knapsack (ERROR)
+	if is_blocking == true:
+		knapsack.append(newRoom)
+		#try again
+	else:
+		# place room
+		newRoom.x = checkingCoordinates.x
+		newRoom.y = checkingCoordinates.y
+		current_location = checkingCoordinates
+		roomstack.append(newRoom)
+
+func is_blocking_exit(room_coords:Vector2,neighbor_coords:Vector2, room_exits:Array):
+	#get room at xy
+	var neighbor:Vector4 = get_room_at_xy(room_coords + neighbor_coords)
+	#get exits of room
+	var neighbor_exits:Array = get_exits(neighbor)
 	
-	# after all rooms are placed
-	# for each room
-		# for each exit
-			# if there is no room there
-				# add a dead end (generate one)
+	var is_blocking:bool = false
 	
-	# after stack is full,
-		# for each room in stack
-			# use 32x32 offset to place room scenes
-			# (rooms should have leftmost corner at 0,0)
-			# (rooms should have centered walkways to connect)
+	#check if blocking neighbor
+	if neighbor_coords.x == -1 and neighbor_exits[0] == 1:
+		#check if current room has path to this room
+		if room_exits[2] != 1:
+			is_blocking = true
+		
+	elif neighbor_coords.x == 1 and neighbor_exits[2] == -1:
+		#check if current room has path to this room
+		if room_exits[0] != 1:
+			is_blocking = true
+		
+	elif neighbor_coords.y == -1 and neighbor_exits[1] == -1:
+		#check if current room has path to this room
+		if room_exits[1] != 1:
+			is_blocking = true
+		
+	elif neighbor_coords.y == 1 and neighbor_exits[3] == 1:
+		#check if current room has path to this room
+		if room_exits[3] != 1:
+			is_blocking = true
+	
+	return is_blocking
+
+func get_room_at_xy(coordinates:Vector2):
+	var x = coordinates.x
+	var y = coordinates.y
+	var room:Vector4
+	
+	for i in range(0,len(roomstack)):
+		if roomstack[i].x == x and roomstack[i].y == y:
+			room = roomstack[i]
+	
+	return room
 
 func get_number_of_exits(room:Vector4):
 	var number_of_exits = 0
@@ -200,8 +286,10 @@ func is_room_in_xy(coordinates:Vector2):
 	var x = coordinates.x
 	var y = coordinates.y
 	
+	var is_room_in_spot = false
 	for i in range(0,len(roomstack)):
 		if roomstack[i].x == x and roomstack[i].y == y:
-			return true
-		else:
-			return false
+			is_room_in_spot = true
+			break
+	
+	return is_room_in_spot
