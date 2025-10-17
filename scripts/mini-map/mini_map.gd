e2910bf588432c734c4f432ebc15cf9bf58ba23c
npnorman <npn.not@gmail.com>
2025-10-17 15:49:58 -0400

    setup mini-map

---
+++ scripts/mini-map/mini_map.gd
@@ -0,0 +1,18 @@
+extends Control
+
+var center_offset = 100
+var mini_room_size = 3
+var player_location = Vector2(0,0)
+
+func convert_room_to_mini():
+	pass
+
+func add_room_as_mini():
+	pass
+
+func set_player_location():
+	pass
+
+func _ready() -> void:
+	#testing purposes
+	pass
