3b26b25b16a8e8c0807ed2b07265cdfb1ce17022
npnorman <npn.not@gmail.com>
2025-11-19 13:55:51 -0500

    basic states

--- scripts/boss/cactus_boss.gd
+++ scripts/boss/cactus_boss.gd
@@ -3,6 +3,7 @@ extends CharacterBody2D
 # arms
 @onready var right_arm: Node2D = $RightArm
 @onready var left_arm: Node2D = $LeftArm
+@onready var phase_1_timer: Timer = $Phase1Timer
 
 var target:Vector2 = Vector2.ZERO
 var origin:Vector2 = Vector2.ZERO
@@ -26,6 +27,21 @@ var phase2 = [
 	Vector2(0,-1)
 ]
 
+# States
+enum States {
+	REST,
+	PHASE1,
+	PHASE2,
+	PHASE3
+}
+var currentState = States.REST
+var isReadyPhase1 = false
+@onready var state_machine_timer: Timer = $StateMachineTimer
+var moveToNextState = true
+
+# Health info
+var hp = 100
+
 func _ready() -> void:
 	#get player
 	player = get_tree().get_first_node_in_group("Player")
@@ -35,6 +51,55 @@ func _ready() -> void:
 
 func _physics_process(delta: float) -> void:
 	
+	# check state
+	checkState()
+	
+	if currentState == States.REST:
+		velocity = Vector2.ZERO
+	elif currentState == States.PHASE1:
+		phase1_pattern()
+	elif currentState == States.PHASE2:
+		phase2_pattern(delta)
+	
+	move_and_slide()
+
+func checkState():
+	# handles enter/exit
+	
+	var moveTime = 10
+	
+	if moveToNextState:
+		
+		moveToNextState = false
+		
+		if currentState == States.REST:
+			# can move to phase 1
+			isReadyPhase1 = true
+			currentState = States.PHASE1
+			
+		elif currentState == States.PHASE1:
+			# move to rest if hp is good
+			if hp > 70:
+				currentState = States.REST
+			else:
+				currentState = States.PHASE2
+				moveTime = 20
+			
+		elif currentState == States.PHASE2:
+			currentState = States.REST
+		
+		state_machine_timer.wait_time = moveTime
+		state_machine_timer.start()
+
+func phase1_pattern():
+	if isReadyPhase1:
+		print("shooting")
+		isReadyPhase1 = false
+		shoot_arm(right_arm)
+		shoot_arm(left_arm)
+		phase_1_timer.start()
+
+func phase2_pattern(delta):
 	if global_position.distance_to(target) < targetDelta:
 		# set new target
 		movementOffset = (movementOffset + 1) % len(phase2)
@@ -42,9 +107,12 @@ func _physics_process(delta: float) -> void:
 		
 		shoot_arm(right_arm)
 		shoot_arm(left_arm)
-	
+		
 	move_to_target(delta)
-	move_and_slide()
+
+func take_damage(damage:int):
+	hp -= damage
+	print("hp: ",hp)
 
 func set_target(target:Vector2):
 	self.target = target
@@ -67,7 +135,6 @@ func shoot(shoot_from_position:Vector2):
 	var rng:RandomNumberGenerator = RandomNumberGenerator.new()
 	var colors = [Color("Yellow"), Color("8c6e00"), Color("ffc64a")]
 	var color_index:int = rng.randi_range(0, len(colors) - 1)
-	print(color_index)
 	
 	var color:Color = colors[color_index]
 	
@@ -86,3 +153,9 @@ func shoot(shoot_from_position:Vector2):
 func shoot_arm(arm):
 	for marker:Marker2D in arm.get_children():
 		shoot(marker.global_position)
+
+func _on_phase_1_timer_timeout() -> void:
+	isReadyPhase1 = true
+
+func _on_state_machine_timer_timeout() -> void:
+	moveToNextState = true
