c6c3dabb7da69120af244da731a0a7877f3c8f4b
Jake Kissler <jakekissler@gmail.com>
2025-12-10 15:01:23 -0500

    Last Minute Fixes
    - Made the whitelist way smaller because Desert generation also happens to be very broken

--- globals/globals.gd
+++ globals/globals.gd
@@ -209,6 +209,7 @@ func change_level(level_scheme:int = current_level):
 	# load level scheme
 	current_level = level_scheme
 	is_boss_transition_room_activated = false
+	isBossTPUnlocked = false
 	get_tree().change_scene_to_file("res://rooms/TestingRoom.tscn")
 	change_music(level_scheme)
 	print(temporary_loadout)
